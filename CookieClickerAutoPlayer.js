7b8d7b10457fecee5ba304c21e86f6b15d6773dd
Daan Kroese <daankroese@gmail.com>
2016-03-24 09:12:15 +0100

    Now displays wrinkler total below player name
    Added functionality that crudely displays the total
    number of cookies eaten by the currently active
    wrinklers below the player's name.
    
    Note: Does not account for the bonus added when
    a wrinkler is popped.

--- CookieClickerAutoPlayer.js
+++ CookieClickerAutoPlayer.js
@@ -1,11 +1,12 @@
 /**
  * Cookie Clicker Optimal Return on Investment Highlighter / Auto Player
-
- * @version            0.2.1
+ * @version            0.3
  * @author             nietdaan
- * @description        Highlights the name of the building with the highest
-                       ROI in green. Can also auto-play the game for you.
- * @usage              Copy-paste into console. Stop with clearInterval(loop);
+ * @description        - Highlights the name of the building with the highest
+                         ROI in green. Can also auto-play the game for you.
+                       - Displays the total number of cookies inside the
+                         currently active wrinklers below your name.
+ * @usage              Copy-paste into console. Stop with CCAP_stop();
                        Alternative: use window.Game.LoadMod(link_to_this_script);
  * @option autoBuy     Set autoBuy to true to let script buy buildings
                        automatically
@@ -13,7 +14,6 @@
                        upgrades automatically.
  * @option logging     Set logging to true to log best option to console
                        each cycle (very resource intensive)
-
  * @info               After rougly 9,5 hours of play with no manual clicks and
                        100 cookies in the bank to start with, this is the result:
                                                -------------------------------
@@ -23,20 +23,22 @@
                        | autoBuy + autoUpgrade |  66.296.531.736 | 5.245.552 |
                        -------------------------------------------------------
  */
- 
+
+// Declare global variables
 var currentHighlight = -1;
 var autoBuy          = false;
 var autoUpgrade      = false;
 var logging          = false;
- 
+var intervals        = {};
+
 function determineOptimalROI() {
     var buyMe = {id: 0, price: 0, ROI: 0, name: ""};
- 
+
     // Loop through buildings and select option with best ROI
     for (var i = 0; i < window.Game.ObjectsById.length; i++) {
         var object = window.Game.ObjectsById[i];
         var ROI    = object.storedCps / object.price;
-         
+
         if (ROI > buyMe.ROI) {
             buyMe.id    = i;
             buyMe.price = object.price;
@@ -44,7 +46,7 @@ function determineOptimalROI() {
             buyMe.name  = object.name;
         }
     }
- 
+
     // Highlight buidling with best ROI
     if (buyMe.id != currentHighlight) {
         if (currentHighlight != -1) {
@@ -53,12 +55,12 @@ function determineOptimalROI() {
         document.getElementById("productName" + buyMe.id).style.color = "#77FF77";
         currentHighlight = buyMe.id;
     }
- 
+
     // Autobuy buidling with best ROI
     if (autoBuy && buyMe.price <= window.Game.cookies) {
         window.Game.ObjectsById[buyMe.id].buy(1);
     }
- 
+
     // Log building with best ROI to console
     if (logging) {
         console.log("Best option: " + buyMe.name + " with ROI " + buyMe.ROI);
@@ -75,8 +77,36 @@ function determineOptimalROI() {
     }
 }
 
-// Execute loop every 100 milliseconds
-var loop = setInterval(determineOptimalROI, 100);
+// Display total number of cookies inside current wrinklers below your name
+function displayWrinklerTotal() {
+    // Add DOM element if necessary
+    if (document.getElementById("wrinklerTotal") === null) {
+        var wrinklerTotal            = document.createElement("div");
+        wrinklerTotal.id             = "wrinklerTotal";
+        wrinklerTotal.style.fontSize = "60%";
+        document.getElementById("bakeryName").appendChild(wrinklerTotal);
+    }
+
+    var total = 0;
+    for (var i = 0; i < window.Game.wrinklers.length; i++) {
+        total += window.Game.wrinklers[i].sucked;
+    }
+    document.getElementById("wrinklerTotal").innerHTML = "Wrinkler total: " + Beautify(total);
+}
+
+// Stop all functionality
+function CCAP_stop() {
+    for (var key in intervals) {
+        clearInterval(intervals[key]);
+    }
+}
+
+// Execute ROI loop every 100 milliseconds
+intervals.ROI = setInterval(determineOptimalROI, 100);
+
+// Execute Wrinkler loop every 10.000 milliseconds
+displayWrinklerTotal();
+intervals.wrinkler = setInterval(displayWrinklerTotal, 10000);
 
 // Win 'Third-party' achievement for using a mod
 window.Game.Win('Third-party');
